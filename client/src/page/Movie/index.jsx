13b1ec08433e53f0cee38f6badf0ddf2ee6d5e47
Suleiman Ramazanow <suleimanramazanow@MacBook-Air-Suleiman.local>
2025-11-25 18:49:52 +0300

    change text

--- client/src/page/Movie/index.jsx
+++ client/src/page/Movie/index.jsx
@@ -73,7 +73,7 @@ const MoviePage = () => {
 
       <div className="mt-8">
         <h2 className="text-2xl font-semibold mb-4 text-center">
-          Где можно посмотреть
+          Когда можно посмотреть
         </h2>
 
         <div className="space-y-3">
